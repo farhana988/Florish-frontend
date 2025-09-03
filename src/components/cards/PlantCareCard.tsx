ad01c1e202f6c1785929ce48fbd2424d62c5cdd1
farhana988 <farhana744255@gmail.com>
2025-09-03 08:50:47 +0600

    updated plant card

---
+++ src/components/cards/PlantCareCard.tsx
@@ -0,0 +1,47 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+interface Tip {
+  title: string;
+  date: string;
+  description: string;
+  image: string;
+}
+interface PlantCareCardProps {
+  tip: Tip;
+}
+
+const PlantCareCard = ({ tip }: PlantCareCardProps) => {
+  return (
+    <Card className="flex flex-col overflow-hidden rounded-xl shadow-sm transition-transform pt-0">
+      <div className="relative w-full h-56">
+        <Image src={tip.image} alt={tip.title} fill className="object-cover" />
+      </div>
+      <CardContent className="flex flex-col flex-grow">
+        <div className="flex-grow">
+          <h3 className="text-xl lg:text-2xl font-semibold text-[#1c1f13] mb-2">
+            {tip.title}
+          </h3>
+          <p className="text-xs text-gray-600 mb-2">{tip.date}</p>
+          <p className="text-sm lg:text-base text-gray-700">
+            {tip.description}
+          </p>
+        </div>
+        <div className="mt-4 text-left">
+          <a
+            href="#"
+            className="hover:underline hover:text-blue-500 whitespace-nowrap
+              flex items-center gap-1 text-gray-600 text-sm"
+          >
+            Continue Reading
+            <ChevronRight size={16} />
+          </a>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PlantCareCard;
---
+++ src/data/plantCareData.ts
@@ -0,0 +1,23 @@
+export const tips = [
+  {
+    title: "How To Pot Plants",
+    date: "May 26, 2022",
+    description:
+      "Discover essential tips to keep your indoor plants thriving, from proper watering to choosing the right soil.",
+    image: "/plant-care1.jpg",
+  },
+  {
+    title: "How Much Sunlight Is Needed",
+    date: "May 26, 2022",
+    description:
+      "Learn how different types of light affect plant growth and how to find the best spot in your home.",
+    image: "/plant-care2.jpg",
+  },
+  {
+    title: "Show Plants",
+    date: "May 26, 2022",
+    description:
+      "Explore unique and stylish ways to showcase your plants around the house for a fresh, natural vibe.",
+    image: "/plant-care3.jpg",
+  },
+];
