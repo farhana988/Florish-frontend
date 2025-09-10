6dc95c317d28563883068299d25bba73a6d49760
farhana988 <farhana744255@gmail.com>
2025-09-10 09:12:30 +0600

    resolved build eror

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Providers from "@/lib/providers";
 import "./globals.css";
 // import { RedirectComponent } from "./redirect";
 
@@ -32,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        {children}
       </body>
     </html>
   );
