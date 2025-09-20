b1d02974cf7f322fd9ddf47b4f5ad82ea777315e
farhana988 <farhana744255@gmail.com>
2025-09-20 22:34:29 +0600

    update

--- src/components/modules/faq/FAQItem.tsx
+++ src/components/modules/faq/FAQItem.tsx
@@ -13,14 +13,13 @@ interface FAQItemProps {
 const FAQItem = ({ value, question, answer }: FAQItemProps) => {
   return (
     <AccordionItem value={value}>
-      <h3>
-        <AccordionTrigger
-          className="flex justify-between items-center w-full p-4
-        text-base md:text-lg"
-        >
-          {question}
-        </AccordionTrigger>
-      </h3>
+      <AccordionTrigger
+        className="flex justify-between items-center w-full p-4 text-base md:text-lg
+        data-[state=closed]:border-b-2 data-[state=closed]:border-dGreen"
+      >
+        {question}
+      </AccordionTrigger>
+
       <AccordionContent>
         <p className="text-gray-500 px-4 text-xs md:text-base">{answer}</p>
       </AccordionContent>
